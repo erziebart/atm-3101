cfa7e99796fa5e5dc9f35eac2104e25068e05698
Sam Muller <sjm2221@columbia.edu>
2018-09-30 20:54:57 -0400

    installed json.net nuget package

--- WindowsFormsApp3/AccountList.cs
+++ WindowsFormsApp3/AccountList.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace WindowsFormsApp3
 {
     public class AccountList
     {
         private readonly string acctFile;
+        private readonly JsonSerializer js;
 
         public AccountList(string s)
         {
             this.acctFile = s;
+            this.js = new JsonSerializer();
         }
+
+        //public Account FindAcct(string acctNum) {
+
+        //    return new Account();
+        //}
     }
 }
