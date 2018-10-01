25115ea9e8b91d32b52093840e5c9deb63321542
Sam Muller <sjm2221@columbia.edu>
2018-09-30 20:31:08 -0400

    created AccountList class file

---
+++ WindowsFormsApp3/AccountList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace WindowsFormsApp3
+{
+    public class AccountList
+    {
+        private readonly string acctFile;
+
+        public AccountList(string s)
+        {
+            this.acctFile = s;
+        }
+    }
+}
