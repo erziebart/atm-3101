2b562613ce1c3faaa4fefab68d51e30d29c7d227
laynebritton <laynebritton1998@gmail.com>
2018-10-06 23:18:33 -0400

    Progress on main menu. Troubleshooting buttons not firing events

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -12,24 +12,38 @@ namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
+        private AccountList accountList;
+        private Account instanceAccount;
         public Form1()
         {
             InitializeComponent();
-
+            this.accountList = new AccountList("accounts.json");
+            this.instanceAccount = accountList.FindAcct("123456781234");
+            // Right now this is hardcoded to use a specific account. Once we have a login protocol, switch out the manual string for whatever is passed
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void Logout()
         {
-
-        }
-
-        public void logout()
-        {
-            Close();
+            //Close();
             //Go back to login
         }
 
-        public void Deposit()
+        public void Deposit(int amount, Account account)
+        {
+            account.Balance += amount;
+            // DepositSlot.AcceptEnvelope()
+            this.accountList.UpdateAcct(account);
+        }
+
+        public void Withdraw(int numberOf20s, Account account)
+        {
+            // ATM.takeOut((numberOf20s * 20));
+            account.Balance -= (numberOf20s * 20);
+            // Dispenser.DispenseCash(numberOf20s_;
+            accountList.UpdateAcct(account);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
