d9eef2def99ca5445b107e0f9c26d6a2292db144
laynebritton <laynebritton1998@gmail.com>
2018-10-07 20:26:19 -0400

    Created interaciton with main menu menu buttons. Basic withdraw and deposit functions exiss.

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -14,23 +14,26 @@ namespace WindowsFormsApp3
     {
         private AccountList accountList;
         private Account instanceAccount;
+
         public Form1()
         {
             InitializeComponent();
             this.accountList = new AccountList("accounts.json");
             this.instanceAccount = accountList.FindAcct("123456781234");
+            label1.Text = this.instanceAccount.Balance.ToString();
             // Right now this is hardcoded to use a specific account. Once we have a login protocol, switch out the manual string for whatever is passed
         }
 
         public void Logout()
         {
-            //Close();
+            Close();
             //Go back to login
         }
 
         public void Deposit(int amount, Account account)
         {
             account.Balance += amount;
+            label1.Text = account.Balance.ToString();
             // DepositSlot.AcceptEnvelope()
             this.accountList.UpdateAcct(account);
         }
@@ -41,11 +44,30 @@ namespace WindowsFormsApp3
             account.Balance -= (numberOf20s * 20);
             // Dispenser.DispenseCash(numberOf20s_;
             accountList.UpdateAcct(account);
+
+            int toWithdraw = (numberOf20s * 20);
+            if (account.Balance - toWithdraw >= 0)
+            {
+                account.Balance -= toWithdraw;
+                label1.Text = account.Balance.ToString();
+                // Dispenser.DispenseCash(numberOf20s);
+                accountList.UpdateAcct(account);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Deposit(60, this.instanceAccount);
+        }
 
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Withdraw(2, this.instanceAccount);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Logout();
         }
     }
 }
